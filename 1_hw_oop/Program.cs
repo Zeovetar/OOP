e13adb3fe73b0dde0ba81593187b00b6cd098938
Константин Артозеев <zeovetar@gmail.com>
2022-01-07 17:36:34 +0300

    1_hw_in_jpg_files

---
+++ 1_hw_oop/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _1_hw_oop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Diagrams all here!");
+        }
+    }
+}
