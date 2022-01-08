e51985e092b0e73baa11f0e71c5ef496309e71b8
Константин Артозеев <zeovetar@gmail.com>
2022-01-08 14:38:26 +0300

    1&2 done clearly

--- 2_hw_oop/Program.cs
+++ 2_hw_oop/Program.cs
@@ -8,14 +8,13 @@ namespace _2_hw_oop
     {
         static void Main(string[] args)
         {
-            Bank1.AccountType accountType = Bank1.AccountType.own;
-            Console.WriteLine(accountType);
             Bank1.BankAccount1 bank1 = new (1, 120000, Bank1.AccountType.bussiness);
             Bank1.BankAccount1 bank2 = new(2, 1200000, Bank1.AccountType.vip);
+            Console.WriteLine("========================================Example_1========================================");
             bank1.GetInfo();
             bank2.GetInfo();
 
-            //Bank2.AccountType accountType2 = Bank2.AccountType.own;
+            Console.WriteLine("========================================Example_2========================================");
             Bank2.BankAccount2 bank3 = new(220000, Bank2.AccountType.bussiness);
             Bank2.BankAccount2 bank4 = new(2200000, Bank2.AccountType.vip);
             Bank2.BankAccount2 bank5 = new(2300000, Bank2.AccountType.own);
