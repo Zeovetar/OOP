6603ebeb3eff1d73c45f96c65fe8ecd012a00ed4
Константин Артозеев <zeovetar@gmail.com>
2022-01-08 14:36:31 +0300

    1&2 done

---
+++ 2_hw_oop/BankAccount2.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace Bank2
+{
+    public enum AccountType { own, bussiness, vip };
+    public class BankAccount2
+    {
+        private int accountNumber;
+        private int balance;
+        private AccountType type;
+        private static int newnumber;
+
+        public BankAccount2(int balance, AccountType type)
+        {
+            newnumber++;
+            accountNumber = newnumber;
+            this.balance = balance;
+            this.type = type;
+        }
+        public int AccountNumber
+        {
+            get
+            {
+                return this.accountNumber;
+            }
+
+            set
+            {
+                this.accountNumber = value;
+            }
+        }
+
+        public int Balance
+        {
+            get
+            {
+                return this.balance;
+            }
+
+            set
+            {
+                this.balance = value;
+            }
+
+        }
+
+        public AccountType Type
+        {
+            get
+            {
+                return this.type;
+            }
+
+            set
+            {
+                this.type = value;
+            }
+        }
+
+        public void GetInfo()
+        {
+            Console.WriteLine($"Номер счета: {this.accountNumber}  Баланс: {this.balance} Тип счета: {type}");
+        }
+    }
+
+}
--- 2_hw_oop/Program.cs
+++ 2_hw_oop/Program.cs
@@ -1,84 +1,27 @@
 ﻿using System;
 using Bank1;
+using Bank2;
 
 namespace _2_hw_oop
 {
-    public class Car
-    {
-        private string model; // модель
-        private int year;    // год
-
-        public void GetInfo()
-        {
-            Console.WriteLine($"Модель: {model}  Год: {year}");
-        }
-
-        /*        public Car(string model, int year)
-                {
-                    this.model = model;
-                    this.year = year;
-                    Console.WriteLine($"Object {this.model} was created at {this.year}.");
-                }*/
-
-        public Car() { model = "Lifan"; year = 1992; }
-
-        public Car(string carModel) : this(carModel, 1993) { }
-
-        public Car(string model, int year) { this.model = model; this.year = year; }
-
-        public string Model
-        {
-            get 
-            {
-                return this.model;
-            }
-
-            set 
-            {
-                this.model = value;
-            }
-            
-        }
-        public int Year
-        {
-            get
-            {
-                return this.year;
-            }
-
-            set
-            {
-                this.year = value;
-            }
-
-        }
-    }
-    //public enum AccountType { own, bussiness, vip };
-
     class Program
     {
         static void Main(string[] args)
         {
-            AccountType accountType = AccountType.own;
+            Bank1.AccountType accountType = Bank1.AccountType.own;
             Console.WriteLine(accountType);
-            Bank1.BankAccount1 bank = new (1, 120_000, AccountType.bussiness);
-            bank.GetInfo();
-            Console.WriteLine(bank.AccountNumber);
-            Console.WriteLine(bank.Balance);
-            Console.WriteLine(bank.Type);
-            //Console.WriteLine(bank);
-            /*            Car lada = new Car("Lada", 2002);
-                        Car yac = new("Yac", 1881);
-                        Car lifan = new("lifan");
-                        Car cherry = new();
-                        cherry.GetInfo();
-                        lifan.GetInfo();
-                        lada.GetInfo();
-                        lada.Model = "Priora";
-                        lada.Year = 1987;
-                        lada.GetInfo();  // Модель: Samara Год: 1990
-                        yac.GetInfo();
-                        Console.ReadKey();*/
+            Bank1.BankAccount1 bank1 = new (1, 120000, Bank1.AccountType.bussiness);
+            Bank1.BankAccount1 bank2 = new(2, 1200000, Bank1.AccountType.vip);
+            bank1.GetInfo();
+            bank2.GetInfo();
+
+            //Bank2.AccountType accountType2 = Bank2.AccountType.own;
+            Bank2.BankAccount2 bank3 = new(220000, Bank2.AccountType.bussiness);
+            Bank2.BankAccount2 bank4 = new(2200000, Bank2.AccountType.vip);
+            Bank2.BankAccount2 bank5 = new(2300000, Bank2.AccountType.own);
+            bank3.GetInfo();
+            bank4.GetInfo();
+            bank5.GetInfo();
         }
     }
 }
