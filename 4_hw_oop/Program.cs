5804b25cdf80e636a5b9e8fc30d31221951beb06
Константин Артозеев <zeovetar@gmail.com>
2022-01-10 23:24:06 +0300

    4_dz_done

---
+++ 4_hw_oop/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Building;
+
+namespace _4_hw_oop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("====================_1_====================");
+            BuildingInfo b1 = new(30,9,128,4);
+            b1.GetInfo();
+            Console.WriteLine("====================_2_====================");
+            BuildingInfo b2 = new(38, 16, 200, 6);
+            b2.GetInfo();
+        }
+    }
+}
