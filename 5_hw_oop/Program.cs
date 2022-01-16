75d57b37961090d8d991b1b2a6836a753a3fc35d
Константин Артозеев <zeovetar@gmail.com>
2022-01-16 14:26:44 +0300

    == !=

---
+++ 5_hw_oop/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using RationalNumbers;
+
+namespace _5_hw_oop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Numbers num = new(1, 2);
+            Numbers num2 = new(4, 8);
+            if (num == num2)
+            {
+                Console.WriteLine($"{num.Numerator}/{num.Denumerator} == { num2.Numerator}/{num2.Denumerator}");
+            }
+            else
+            {
+                Console.WriteLine($"{num.Numerator}/{num.Denumerator} != { num2.Numerator}/{num2.Denumerator}");
+            }
+/*            bool i = true;
+            Console.WriteLine($"I= {i = (num == num2)}");
+            Console.WriteLine($"Numerator: {num.Numerator} Denumerator: {num.Denumerator}");
+            Console.WriteLine($"Numerator: {num2.Numerator} Denumerator: {num2.Denumerator}");*/
+        }
+    }
+}
---
+++ 5_hw_oop/RationalNumbers.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace RationalNumbers
+	{
+
+	public class Numbers
+	{
+		private int numerator;
+		private int denumerator;
+
+		public int Numerator { get { return this.numerator; } set { this.numerator = value; } }
+		public int Denumerator { get { return this.denumerator; } set { this.numerator = value; } }
+		public Numbers(int numerator, int denumerator)
+		{
+			this.numerator = numerator;
+			this.denumerator = denumerator;
+		}
+
+		public static void nod(Numbers fraction1, Numbers fraction2)
+		{
+			for (int i = 2; i < 10; i++)
+			{
+				if (fraction1.numerator % i == 0 && fraction1.denumerator % i == 0 && fraction2.numerator == 0 && fraction2.denumerator == 0)
+				{
+					fraction1.numerator = fraction1.numerator / i;
+					fraction1.denumerator = fraction1.denumerator / i;
+					fraction2.numerator = fraction2.numerator / i;
+					fraction2.denumerator = fraction2.denumerator / i;
+					nod(fraction1, fraction2);
+				}
+			}
+		}
+
+		public static void nok(Numbers fraction1, Numbers fraction2)
+		{
+			int nok1 = fraction1.denumerator;
+			int nok2 = fraction2.denumerator;
+			fraction1.numerator = fraction1.numerator * nok2;
+			fraction1.denumerator = fraction1.denumerator * nok2;
+			fraction2.numerator = fraction2.numerator * nok1;
+			fraction2.denumerator = fraction2.denumerator * nok1;
+		}
+
+		public static bool operator ==(Numbers fraction1, Numbers fraction2)
+		{
+			//nod(fraction1, fraction2);
+			nok(fraction1, fraction2);
+			if (fraction1.numerator / fraction2.numerator == 1 && fraction1.denumerator / fraction2.denumerator == 1)
+			{
+				return true;
+			}
+			return false;
+		}
+
+		public static bool operator !=(Numbers fraction1, Numbers fraction2)
+		{
+			if (fraction1 == fraction2)
+			{
+				return false;
+			}
+			return true;
+		}
+
+		public static bool Equals(object obj)
+		{
+			return true;
+		}
+
+		public static bool GetHashCode()
+		{
+			return true;
+		}
+	}
+}
