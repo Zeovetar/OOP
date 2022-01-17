482644480d24ceac684d7d9beb5ef03e72bb5dcd
Zeovetar <zeovetar@gmail.com>
2022-01-17 20:54:06 +0300

    stage

--- 5_hw_oop/Program.cs
+++ 5_hw_oop/Program.cs
@@ -9,7 +9,7 @@ namespace _5_hw_oop
         {
             Console.WriteLine("Hello World!");
             Numbers num = new(1, 2);
-            Numbers num2 = new(4, 8);
+            Numbers num2 = new(3, 8);
             if (num == num2)
             {
                 Console.WriteLine($"{num.Numerator}/{num.Denumerator} == { num2.Numerator}/{num2.Denumerator}");
@@ -18,10 +18,31 @@ namespace _5_hw_oop
             {
                 Console.WriteLine($"{num.Numerator}/{num.Denumerator} != { num2.Numerator}/{num2.Denumerator}");
             }
-/*            bool i = true;
-            Console.WriteLine($"I= {i = (num == num2)}");
-            Console.WriteLine($"Numerator: {num.Numerator} Denumerator: {num.Denumerator}");
-            Console.WriteLine($"Numerator: {num2.Numerator} Denumerator: {num2.Denumerator}");*/
+            if (num.Equals(num2))
+            {
+                Console.WriteLine("Equals");
+            }
+            else
+            {
+                Console.WriteLine("not Equals");
+            }
+            //====================================================================================================
+            Numbers sum = new(0, 1);
+            sum = num + num2;
+            Console.WriteLine($"Summa: {sum.Numerator} {sum.Denumerator}");
+            //====================================================================================================
+            num++;
+            num2--;
+            Console.WriteLine($"++: {num.Numerator}/{num.Denumerator}");
+            Console.WriteLine($"--: {num2.Numerator}/{num2.Denumerator}");
+            //====================================================================================================
+            Console.WriteLine($"<?: {num < num2}");
+            Console.WriteLine($"Ю?: {num > num2}");
+            //====================================================================================================
+            Numbers num3 = new(1, 2);
+            Numbers num4 = new(2, 4);
+            Console.WriteLine($"<=?: {num3 < num4}");
+            Console.WriteLine($">=?: {num3 > num4}");
         }
     }
 }
