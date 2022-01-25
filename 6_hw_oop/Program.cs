bd3fcd753f91ebad30df7984943e0200b01f8c89
Константин Артозеев <zeovetar@gmail.com>
2022-01-25 21:53:04 +0300

    6 hw done

---
+++ 6_hw_oop/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Bank;
+
+namespace _6_hw_oop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            BankAccount acc1 = new(1,300_000);
+            BankAccount acc2 = new(1,400_000);
+            if (acc1 == acc2)
+            {
+                Console.WriteLine("==");
+            }
+            else
+            {
+                Console.WriteLine("!=");
+            }
+            if (acc1.Equals(acc2))
+            {
+                Console.WriteLine("Equals");
+            }
+            else 
+            {
+                Console.WriteLine("notEquals");
+            }
+            Console.WriteLine($"HashCode: {acc1.GetHashCode()}");
+            string test = acc1.ToString();
+            Console.WriteLine(test);
+        }
+    }
+}
