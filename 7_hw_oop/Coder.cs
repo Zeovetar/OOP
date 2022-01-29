85e11b63dcc1bd1762a0a19fe152c7f11ec1c7ce
Константин Артозеев <zeovetar@gmail.com>
2022-01-29 18:53:52 +0300

    7_hw_oop done without checking "я+"

---
+++ 7_hw_oop/Coder.cs
@@ -0,0 +1,95 @@
+﻿using System;
+
+namespace Coder
+{
+	interface ICoder
+	{
+		public string Encode(string inputString);
+
+		public string Decode(string inputString);
+	}
+    public class ACoder : ICoder
+    {
+        private bool checkStr(char[] chr)
+        {
+            bool pr = true;
+            foreach (char ch in chr)
+            {
+                if ((int)ch < 1072 || (int)ch > 1104)
+                {
+                    pr =  false;
+                }
+            }
+            return pr;
+        }
+        public string Decode(string inputString)
+        {
+            char[] charArr = inputString.ToCharArray();
+            if (checkStr(charArr))
+            {
+                for (int i = 0; i < charArr.Length; i++)
+                {
+                    int ch = (int)charArr[i] - 1;
+                    charArr[i] = (char)ch;
+                }
+            }
+                return new string(charArr);
+        }
+
+        public string Encode(string inputString)
+        {
+            char[] charArr = inputString.ToCharArray();
+            if (checkStr(charArr))
+            {
+                for (int i = 0; i < charArr.Length; i++)
+                {
+                    int ch = (int)charArr[i] + 1;
+                    charArr[i] = (char)ch;
+                }
+            }
+            return new string(charArr);
+        }
+    }
+    public class BCoder : ICoder
+    {
+        private bool checkStr(char[] chr)
+        {
+            bool pr = true;
+            foreach (char ch in chr)
+            {
+                if ((int)ch < 1072 || (int)ch > 1104)
+                {
+                    pr = false;
+                }
+            }
+            return pr;
+        }
+        public string Decode(string inputString)
+        {
+            char[] charArr = inputString.ToCharArray();
+            if (checkStr(charArr))
+            {
+                for (int i = 0; i < charArr.Length; i++)
+                {
+                    int counter = 1104 - (int)charArr[i];
+                    charArr[i] = (char)(1072 + counter);
+                }
+            }
+            return new string(charArr);
+        }
+
+        public string Encode(string inputString)
+        {
+            char[] charArr = inputString.ToCharArray();
+            if (checkStr(charArr))
+            {
+                for (int i = 0; i < charArr.Length; i++)
+                {
+                    int counter = (int)charArr[i] - 1072;
+                    charArr[i] = (char)(1104 - counter);
+                }
+            }
+            return new string(charArr);
+        }
+    }
+}
